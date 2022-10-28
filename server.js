d391790504763e58b1991c480ab6b73004460db5
cathygarciapinon <cathygarciapinon@gmail.com>
2022-10-28 11:53:08 -0700

    Method Override installed, required, and mounted

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 
 require('dotenv').config();
 require('./config/database');
@@ -11,6 +12,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +24,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(methodOverride('_method'));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
